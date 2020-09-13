8130b362c8389b019657430c2a1d9339be1e8302
Brian Webb <webbbn@gmail.com>
2020-09-12 18:10:34 -0700

    Removed an unused function.

--- src/raw_socket.cc
+++ src/raw_socket.cc
@@ -231,40 +231,6 @@ bool set_wifi_monitor_mode(const std::string &device) {
   return false;
 }
 
-#if 0
-bool set_wifi_txpower_fixed(const std::string &device) {
-
-  /* Create the socket and connect to it. */
-  struct nl_sock *sckt = nl_socket_alloc();
-  genl_connect(sckt);
-
-  /* Allocate a new message. */
-  struct nl_msg *mesg = nlmsg_alloc();
-
-  /* Check /usr/include/linux/nl80211.h for a list of commands and attributes. */
-  enum nl80211_commands command = NL80211_CMD_SET_WIPHY;
-
-  /* Create the message so it will send a command to the nl80211 interface. */
-  genlmsg_put(mesg, 0, 0, genl_ctrl_resolve(sckt, "nl80211"), 0, 0, command, 0);
-
-  /* Add specific attributes to change the frequency of the device. */
-  NLA_PUT_U32(mesg, NL80211_ATTR_IFINDEX, if_nametoindex(device.c_str()));
-  NLA_PUT_U32(mesg, NL80211_ATTR_WIPHY_TX_POWER_SETTING, NL80211_TX_POWER_FIXED);
-
-  /* Finally send it and receive the amount of bytes sent. */
-  nl_send_auto_complete(sckt, mesg);
-
-  nlmsg_free(mesg);
-  nl_socket_free(sckt);
-  return true;
-
- nla_put_failure:
-  nlmsg_free(mesg);
-  nl_socket_free(sckt);
-  return false;
-}
-#endif
-
 bool set_wifi_txpower(const std::string &device, uint32_t power_mbm) {
 
   // Write the scaled tx power level to the device register if it exists.
